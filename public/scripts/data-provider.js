590b30f62f8eb9a818fbaa54b53dbe59774e3d6f
junha <81312429+0c0white@users.noreply.github.com>
2024-05-09 10:20:15 +0900

    Delete public directory

--- public/scripts/data-provider.js
+++
@@ -1,8 +0,0 @@
-const path = require("path");
-const fs = require("fs");
-// for now, we will get our data by reading the provided json file
-const file = process.env.DATAFILE;
-module.exports = {
-    filename: file,
-    data: stocks
-    };
